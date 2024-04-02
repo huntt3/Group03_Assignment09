5ab0e9307ee25238c445b7dbf0c07399ab07b157
Trehu <Trehu@LAPTOP-2ALAK15K.home>
2024-04-01 23:00:52 -0400

    Added getDrinkDict(URL) function and called it.

---
+++ cocktailsPackage/cocktails.py
@@ -0,0 +1,28 @@
+#cocktails.py
+#***********************************************************************************************************
+# Names: Trevor Hunt, [], []                                                                               *
+# emails: huntt3@mail.uc.edu, [], []                                                                       *
+# Assignment Number: Assignment 09                                                                         *
+# Due Date: 04/04/2024                                                                                     *
+# Course/Section: IS4010-001                                                                               *
+# Semester/Year: Spring 2024                                                                               *
+# Brief Description of the assignment: Demonstrates usage of APIs, and dictionaries                        *
+#                                                                                                          *
+# Brief Description of what this module does: Defines functions to extract data from database using an API *
+# Citations: https://www.thecocktaildb.com/api.php                                                         *
+# Anything else that's relevant:                                                                           *
+#***********************************************************************************************************
+import requests
+import json
+def getDrinkDict(URL):
+    '''
+    Given API URL, retrieves data in a JSON file, and parses into a dictionary
+    @param URL: API URL from desired database
+    @return: Results from API in a Python dictionary
+    '''
+    response = requests.get(URL)
+    
+    json_string = response.content
+    
+    parsed_json = json.loads(json_string)   # Now we have a python dictionary
+    return(parsed_json)
--- mainPackage/main.py
+++ mainPackage/main.py
@@ -1,26 +1,37 @@
 #main.py
-#*************************************************************************************************************************************
-# Names: Trevor Hunt, [], []                                                                                     *
-# emails: huntt3@mail.uc.edu, [], []                                                                                                *
-# Assignment Number: Assignment 08                                                                                                   *
-# Due Date: 04/04/2024                                                                                                               *
-# Course/Section: IS4010-001                                                                                                         *
-# Semester/Year: Spring 2024                                                                                                         *
-# Brief Description of the assignment: [insert description here]                           *
-#                                                                                                                                    *
-# Brief Description of what this module does: [insert description here]*
-# Citations: https://github.com/apilayer/restcountries                                                                               *
-# Anything else that's relevant:                                                                                                     *
-#*************************************************************************************************************************************\
+#*********************************************************************************************************
+# Names: Trevor Hunt, [], []                                                                             *
+# emails: huntt3@mail.uc.edu, [], []                                                                     *
+# Assignment Number: Assignment 09                                                                       *
+# Due Date: 04/04/2024                                                                                   *
+# Course/Section: IS4010-001                                                                             *
+# Semester/Year: Spring 2024                                                                             *
+# Brief Description of the assignment: Demonstrates usage of APIs, and dictionaries                      *
+#                                                                                                        *
+# Brief Description of what this module does: Calls functions to extract data from database using an API *
+# Citations: https://www.thecocktaildb.com/api.php                                                       *
+# Anything else that's relevant:                                                                         *
+#*********************************************************************************************************
+'''
+    To Do:
+        Extract some interesting data from the dictionary and print it to the console in a friendly
+    and informative format
+        Finish Documentation
+        Finish Modularity
+        
+'''
+from cocktailsPackage.cocktails import *
 import requests
 import json
+from cocktailsPackage.cocktails import getDrinkDict
 if __name__ == "__main__":
-    response = requests.get('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=pfJKDXPzTykVL73ehnPyY8pkDQLjfq5cz5LqCkl3')
-    json_string = response.content
-    parsed_json = json.loads(json_string) # Now we have a python dictionary
-    #print(parsed_json)
-    #print(parsed_json['data'][0]['description'])
-    #print(parsed_json['data'][0]['directionsInfo'])
-    total = int(parsed_json['total']) # The number of parks that were returned
-    for park in parsed_json['data']:
-        print (park)
+    #Note: On the given links, change the "www." to "https://"
+    
+    #parsed_json = getDrinkDict('https://thecocktaildb.com/api/json/v1/1/lookup.php?i=11007')
+    #parsed_json = getDrinkDict('https://thecocktaildb.com/api/json/v1/1/filter.php?g=Champagne_flute')
+    eggDrinks = getDrinkDict('https://thecocktaildb.com/api/json/v1/1/filter.php?i=egg')
+
+    for drink in eggDrinks["drinks"]:     #Iterate though python dictionary
+        print (drink)
+  
+    print("There are" , len(eggDrinks["drinks"]) , "drinks that contain egg")
