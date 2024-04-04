7dacdec7884ea938ff3ba7b729f42f0d7a95abd3
Trehu <Trehu@LAPTOP-2ALAK15K.wifi.uc.edu>
2024-04-03 21:01:44 -0400

    Removed "To Do" List, and added documentation

--- mainPackage/main.py
+++ mainPackage/main.py
@@ -1,7 +1,7 @@
 #main.py
 #*********************************************************************************************************
-# Names: Trevor Hunt, Aimee Madrigal, Rhodas Yemaneab                                                                        *
-# emails: huntt3@mail.uc.edu, ignaciac@mail.uc.edu, yemanert@mail.uc.edu                                                                   *
+# Names: Trevor Hunt, Aimee Madrigal, Rhodas Yemaneab                                                    *
+# emails: huntt3@mail.uc.edu, ignaciac@mail.uc.edu, yemanert@mail.uc.edu                                 *
 # Assignment Number: Assignment 09                                                                       *
 # Due Date: 04/04/2024                                                                                   *
 # Course/Section: IS4010-001                                                                             *
@@ -12,20 +12,13 @@
 # Citations: https://www.thecocktaildb.com/api.php                                                       *
 # Anything else that's relevant:                                                                         *
 #*********************************************************************************************************
-'''
-    To Do:
-        Extract some interesting data from the dictionary and print it to the console in a friendly
-    and informative format
-        Finish Documentation
-        Finish Modularity
-        
-'''
+
 from cocktailsPackage.cocktails import *
 import requests
 import json
 if __name__ == "__main__":
     #Note: On the given links, change the "www." to "https://"
-    
+    #API key is "1"
     champangeFlute = getDrinkDict('https://thecocktaildb.com/api/json/v1/1/filter.php?g=Champagne_flute')
     eggDrinks = getDrinkDict('https://thecocktaildb.com/api/json/v1/1/filter.php?i=egg')
     
